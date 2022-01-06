148d2e373a34a98badcc802eb99290c50f938897
stew anoya <stewartanoya@gmail.com>
2022-01-06 16:25:05 +0000

    Redirect to root instead of login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to :root
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    # Might change to root, since visitors and users can view entire site.
+    redirect_to :root
+  end
+
 end

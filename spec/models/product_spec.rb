34e569380d2b397bb146d089e0969393f0093408
stew anoya <stewartanoya@gmail.com>
2022-01-10 19:36:52 +0000

    write tests for Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,90 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+
+    it 'will save if every field is filled' do      
+      category = Category.new name: "food"
+      category.save
+
+      product = category.products.create({
+        name:'Hamburger',
+        description: 'is delicious',
+        quantity: 1,
+        price: 2.89,
+      })
+      expect(product).to be_valid
+    end
+ 
+    
+    it 'validates presence of name' do
+      category = Category.new name: "food"
+      category.save
+
+      product = category.products.create({
+        name: nil,
+        description: 'is delicious',
+        quantity: 1,
+        price: 2.89,
+      })
+      expect(product).to_not be_valid
+      puts ""
+      print "looking for expect error in array -> " 
+      print product.errors.full_messages
+    end
+
+    it 'validates presence of price' do
+      category = Category.new name: "food"
+      category.save
+
+      product = category.products.create({
+        name: "Hamburger",
+        description: 'is delicious',
+        quantity: 1,
+        price: nil,
+      })
+      expect(product).to_not be_valid
+      puts ""
+      print "looking for expect error in array -> " 
+      print product.errors.full_messages
+
+    end
+
+    it 'validates presence of quantity' do
+      category = Category.new name: "food"
+      category.save
+
+      product = category.products.create({
+        name: "Hamburger",
+        description: 'is delicious',
+        quantity: nil,
+        price: 2.89,
+      })
+      expect(product).to_not be_valid
+      puts ""
+      print "looking for expect error in array -> " 
+      print product.errors.full_messages
+    end
+
+    it 'validates presence of category' do
+      category = Category.new name: "food"
+      category.save
+
+      product = Product.create({
+        name: "Hamburger",
+        description: 'is delicious',
+        quantity: 1,
+        price: 2.89,
+        category: nil
+      })
+      expect(product).to_not be_valid
+      puts ""
+      print "looking for expect error in array -> " 
+      print product.errors.full_messages
+   end
+
+
+  end
+
+end
